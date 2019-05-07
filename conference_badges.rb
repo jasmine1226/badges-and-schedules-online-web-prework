206f083a7a360564408c7071204511143ae0b08e
Jasmine Chen <jasmine1226@gmail.com>
2019-05-07 02:54:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batcg_badge_creator(namelist)
+def batch_badge_creator(namelist)
   namelist.each do |name|
     return "Hello, my name is #{name}."
   end
