bc4a08328dd4568ebc28f3dce4d1ae1c98a9df80
Jasmine Chen <jasmine1226@gmail.com>
2019-05-07 03:06:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,11 +5,11 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(namelist)
-  batch_badge = []
+  batch_badges = []
   namelist.each do |name|
-    batch_badge.push("Hello, my name is #{name}.")
+    batch_badges.push("Hello, my name is #{name}.")
   end
-  return batch_badge
+  return batch_badges
 end
 
 def assign_rooms(namelist)
@@ -19,4 +19,15 @@ def assign_rooms(namelist)
     room_assignments.push("Hello, #{name}! You'll be assigned to room #{room_num}!")
   end
   return room_assignments
+end
+
+def printer(namelist)
+  batch_badges = batch_badge_creator(namelist)
+  room_assignments = assign_rooms(namelist)
+  batch_badges.each do |badge|
+    puts badge
+  end
+  room_assignments.each do |room|
+    puts room
+  end
 end
